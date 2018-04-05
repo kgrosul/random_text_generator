b7f0419a77b151194d35a0e0f8b75bd71139a14a
kgrosul <34602954+kgrosul@users.noreply.github.com>
2018-04-05 21:19:41 +0300

    Update train.py

--- train.py
+++ train.py
@@ -5,11 +5,11 @@ import pickle
 import re
 
 
-def file_train(main_dict, input_file, word_num, lowercase):
+def file_train(main_dict, input_file, words_num, lowercase):
     """
     :param main_dict: словарь, содержащий модели
     :param input_file: файл с текстом
-    :param word_num: количество слов, на основании которых выбирается следующее
+    :param words_num: длина н-грамма - 1
     :param lowercase: надо ли приводить к lowercase
 
     Функция посстрочно считывает текст из заданного файла.
@@ -34,7 +34,7 @@ def file_train(main_dict, input_file, word_num, lowercase):
         for i in range(len(words) - 1):
             words_list.append(words[i])
             next_word = words[i+1]
-            if len(words_list) < word_num:
+            if len(words_list) < words_num:
                 continue
             words_tuple = tuple(words_list)
             if words_tuple not in main_dict.keys():
@@ -53,7 +53,7 @@ def train(input_dir, model_file, words_num, lowercase=False):
     """
     :param input_dir: директория, где лежат тексты
     :param model_file: файл, где хранится модель
-    :param words_num: количество слов, на основании которых выбирается следующее
+    :param words_num: длина н-грамма - 1
     :param lowercase: приводить ли к lowercase
 
     Для каждого файла в заданной директории вызывается функция
@@ -95,7 +95,7 @@ if __name__ == '__main__':
     parser.add_argument("--input-dir", type=str, default=None, help="path to the input directory")
     required.add_argument("--model", type=str, help="path to to the model file",required=True)
     parser.add_argument("--lc", action='store_true', default=False, help="converting text to the lower case")
-    parser.add_argument("--word-num", type=int, default=1, help="length of a Markov chain")
+    parser.add_argument("--words-num", type=int, default=1, help="length of a Markov chain")
     args = parser.parse_args()
 
-    train(args.input_dir, args.model, args.word_num, args.lc)
+    train(args.input_dir, args.model, args.words_num, args.lc)
