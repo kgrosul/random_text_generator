2e6cc582811b9704c9806de5aa938971b9c2befb
Kirill Grosul <kirill.grosul@gmail.com>
2018-04-07 08:06:51 +0300

    Code style correction

--- generate.py
+++ generate.py
@@ -39,7 +39,7 @@ def generate(model_file, seed, length, output_file):
         if cur_words not in model_dict.keys():
             """
             Если необходимого набора слов нет в ключах словаря из модели,
-            то за новый набор возьмем его случайный ключ, а за новое слово - 
+            то за новый набор возьмем его случайный ключ, а за новое слово -
             последнее слово данного набора.
             """
             cur_words = random.choice(list(model_dict.keys()))
@@ -50,12 +50,15 @@ def generate(model_file, seed, length, output_file):
                 tmp_words += [key]
                 tmp_p += [model_dict[cur_words][key]]
 
-            cur_words = cur_words[1:] + (numpy.random.choice(tmp_words, p=tmp_p),)
+            cur_words = cur_words[1:] + (numpy.random.choice(tmp_words,
+                                                             p=tmp_p
+                                                             ),)
         text += cur_words[-1] + ' '
         if (i + 1) % MAX_WORDS_NUM == 0:
             """
-            Если в переменной text уже достаточно много слов(а именно MAX_WORDS_NUM),
-            выведем их в файл, а значение text обнулим
+            Если в переменной text уже достаточно много
+            слов(а именно MAX_WORDS_NUM), выведем их в файл,
+            а значение text обнулим
             """
             output.write(text)
             text = ''
@@ -64,16 +67,36 @@ def generate(model_file, seed, length, output_file):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description=
-                                     "Generates texts using model made by train.py."
-                                     "You can set length of the text, file contains model, "
-                                     "output file and the the first word.")
+    parser = argparse.ArgumentParser(
+        description="Generates texts using model made by train.py."
+                    "You can set length of the text, file contains model, "
+                    "output file and the the first word.")
 
     required = parser.add_argument_group('required arguments')
 
-    required.add_argument("--model", type=str, help="path to the model", required=True)
-    parser.add_argument("--seed", type=str, default=None, help="the first word")
-    required.add_argument("--length", type=int, help="length of the text", required=True)
-    parser.add_argument("--output", type=str, default=None, help="output file")
+    required.add_argument("--model",
+                          type=str,
+                          help="path to the model",
+                          required=True
+                          )
+
+    parser.add_argument("--seed",
+                        type=str,
+                        default=None,
+                        help="the first word"
+                        )
+
+    required.add_argument("--length",
+                          type=int,
+                          help="length of the text",
+                          required=True
+                          )
+
+    parser.add_argument("--output",
+                        type=str,
+                        default=None,
+                        help="output file"
+                        )
+
     args = parser.parse_args()
     generate(args.model, args.seed, args.length, args.output)
--- train.py
+++ train.py
@@ -11,17 +11,19 @@ def normalize(main_dict):
     for key1 in main_dict.keys():
         tmp_sum = sum(main_dict[key1].values())
         for key2 in main_dict[key1].keys():
-            main_dict[key1][key2] /= tmp_sum
+            main_dict[key1][key2] /= \
+                tmp_sum
 
 
 def file_train(main_dict, input_file, words_num, lowercase):
     """
     :param main_dict: словарь, содержащий модели
     :param input_file: файл с текстом
-    :param words_num: количество слов, на основании которых выбирается следующее
+    :param words_num: количество слов,
+                    на основании которых выбирается следующее
     :param lowercase: надо ли приводить к lowercase
 
-    Функция посстрочно считывает текст из заданного файла.
+    Функция построчно считывает текст из заданного файла.
     Каждая строка разбивается при необходимосто приводится
     к lowercase, затем разбивается на слова, состоящие только
     из алфавитных символов. Для каждо подстроки вида
@@ -56,7 +58,8 @@ def train(input_dir, model_file, words_num, lowercase=False):
     """
     :param input_dir: директория, где лежат тексты
     :param model_file: файл, где хранится модель
-    :param words_num: количество слов, на основании которых выбирается следующее
+    :param words_num: количество слов,
+                    на основании которых выбирается следующее
     :param lowercase: приводить ли к lowercase
 
     Для каждого файла в заданной директории вызывается функция
@@ -83,18 +86,38 @@ def train(input_dir, model_file, words_num, lowercase=False):
     with open(model_file, 'wb') as output:
         pickle.dump(dict(main_dict), output)
 
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description=
-                                     "Build model for generate.py using your texts. "
-                                     "You can set input directory, file name for the model,"
-                                     "n in n-gramm and should the text be lowercase or not")
+    parser = argparse.ArgumentParser(
+        description="Build model for generate.py using your texts. "
+                    "You can set input directory, file name for the model,"
+                    "n in n-gramm and should the text be lowercase or not")
 
     required = parser.add_argument_group('required arguments')
 
-    parser.add_argument("--input-dir", type=str, default=None, help="path to the input directory")
-    required.add_argument("--model", type=str, help="path to to the model file",required=True)
-    parser.add_argument("--lc", action='store_true', default=False, help="converting text to the lower case")
-    parser.add_argument("--words-num", type=int, default=1, help="length of a Markov chain")
+    parser.add_argument("--input-dir",
+                        type=str,
+                        default=None,
+                        help="path to the input directory"
+                        )
+
+    required.add_argument("--model",
+                          type=str,
+                          help="path to to the model file",
+                          required=True
+                          )
+
+    parser.add_argument("--lc",
+                        action='store_true',
+                        default=False,
+                        help="converting text to the lower case"
+                        )
+
+    parser.add_argument("--words-num",
+                        type=int,
+                        default=1,
+                        help="length of a Markov chain"
+                        )
     args = parser.parse_args()
 
     train(args.input_dir, args.model, args.words_num, args.lc)
