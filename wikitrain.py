180b8f51bcb5131eed17a8691d88be19275c9d77
Kirill Grosul <kirill.grosul@gmail.com>
2018-04-07 07:58:29 +0300

    Code style correction

--- wikitrain.py
+++ wikitrain.py
@@ -11,7 +11,8 @@ def wiki_train(model_file, texts_num, words_num, lowercase=False):
 
     :param model_file: файл для хранения модели
     :param texts_num: количество текстов
-    :param words_num: количество слов, на основании которых выбирается следующее
+    :param words_num: количество слов, на основании которых
+                    выбирается следующее
     :param lowercase: приводить ли к lowercase
 
     Функция строит модель используя texts_num(количество) случайно
@@ -22,7 +23,8 @@ def wiki_train(model_file, texts_num, words_num, lowercase=False):
 
     file = open(model_file, 'w')
     for i in range(texts_num):
-        file.write(wikipedia.page(wikipedia.search(wikipedia.random())[-1]).content)
+        file.write(wikipedia.page(wikipedia.search(
+            wikipedia.random())[-1]).content)
     file.close()
     main_dict = collections.defaultdict(
         lambda: collections.defaultdict(int))
@@ -34,17 +36,38 @@ def wiki_train(model_file, texts_num, words_num, lowercase=False):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description=
-                                     "Build model for generate.py using random texts from Wikipedia. "
-                                     "You can set model file name, number of texts,"
-                                     "n in n-gramm and should the text be lowercase or not")
+    parser = argparse.ArgumentParser(
+        description="Build model for generate.py using random texts "
+                    "from Wikipedia. You can set model file name, number "
+                    "of texts,n in n-gramm and should the text be lowercase "
+                    "or not"
+    )
 
     required = parser.add_argument_group('required arguments')
 
-    parser.add_argument("--texts-num", type=int, default=1,help="number of texts from Wikipedia")
-    required.add_argument("--model", type=str, help="path to to the model file", required=True)
-    parser.add_argument("--lc", action='store_true', default=False, help="converting text to the lower case")
-    parser.add_argument("--words-num", type=int, default=1, help="length of a Markov chain")
-    args = parser.parse_args()
+    parser.add_argument("--texts-num",
+                        type=int,
+                        default=1,
+                        help="number of texts from Wikipedia"
+                        )
 
+    required.add_argument("--model",
+                          type=str,
+                          help="path to to the model file",
+                          required=True
+                          )
+
+    parser.add_argument("--lc",
+                        action='store_true',
+                        default=False,
+                        help="converting text to the lower case"
+                        )
+
+    parser.add_argument("--words-num",
+                        type=int,
+                        default=1,
+                        help="length of a Markov chain"
+                        )
+
+    args = parser.parse_args()
     wiki_train(args.model, args.texts_num, args.words_num, args.lc)
